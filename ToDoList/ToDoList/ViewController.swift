de022b7bc53c451ff6818ec8620884812e8063d0
ne1ghbor <e155731@yogi.local>
2017-01-12 10:55:46 +0900

    todolist修正

--- ToDoList/ToDoList/ViewController.swift
+++ ToDoList/ToDoList/ViewController.swift
@@ -12,61 +12,12 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     @IBOutlet weak var todolistTable: UITableView!
     
-    @IBAction func testedit(_ sender: UIBarButtonItem) {
-        func setEditing(editing: Bool, animated: Bool) {
-            super.setEditing(editing, animated: animated)
-            todolistTable.isEditing = editing
-        }
-    }
-    
-   	
-    @IBAction func testbtn(_ sender: Any) {
-        
-        
-//        if(todolistTable.isEditing == true) {
-//            todolistTable.isEditing = false
-//        } else {
-//            todolistTable.isEditing = true
-//        }
-//        
-        
-    }
-    override func setEditing(_ editing: Bool, animated: Bool) {
-        super.setEditing(editing, animated: animated)
-        
-        // TableViewを編集可能にする
-        todolistTable.setEditing(editing, animated: true)
-        
-        // 編集中のときのみaddButtonをナビゲーションバーの左に表示する
-        if editing {
-            print("編集中")
-//            let addButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.add, target: self, action: #selector(ViewController.addCell(sender:)))
-//            self.navigationItem.setLeftBarButton(addButton, animated: true)
-        } else {
-            print("通常モード")
-            self.navigationItem.setLeftBarButton(nil, animated: true)
-        }
-    }
-    
-        
-//        if todolistTable.isEditing == true {
-//            todolistTable.isEditing = false
-//        }else{
-//            todolistTable.isEditing = true
-//        }
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         if UserDefaults.init().object(forKey: "todoList") != nil {
             todoItem = UserDefaults.init().object(forKey: "todoList") as! [String]
         }
-        
-//        self.navigationItem.leftBarButtonItem = self.editButtonItem
-//        self.navigationItem.title = "テスト"
-        
-        
     }
     
     //配列のカウント
@@ -83,13 +34,13 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     //リストの削除
-//    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath){
-//        if editingStyle == UITableViewCellEditingStyle.delete{
-//            todoItem.remove(at: indexPath.row)
-//            tableView.deleteRows(at: [indexPath], with: .fade)
-//            UserDefaults.init().set(todoItem, forKey: "todoList")
-//        }
-//    }
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath){
+        if editingStyle == UITableViewCellEditingStyle.delete{
+            todoItem.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            UserDefaults.init().set(todoItem, forKey: "todoList")
+        }
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
