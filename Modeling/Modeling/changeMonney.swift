fd4d04ed47254dc5beebf7cd05abd5207b470700
Masaaki Matsunaga <castle2164m@gmail.com>
2017-02-19 18:04:07 +0900

    home画面とExcangeMonneyをリンク

---
+++ Modeling/Modeling/changeMonney.swift
@@ -0,0 +1,27 @@
+//
+//  changeMonney.swift
+//  Modeling
+//
+//  Created by Masaaki Matsunaga on 2017/02/14.
+//  Copyright © 2017年 RyukyuUniversity. All rights reserved.
+//
+
+import UIKit
+
+class changeMonney: UIViewController{
+    
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+        //Do any additional setup after loading the view,from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        //Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+}
+
