f5415239b4327e75383af9de60e590991360f47f
mamenitro <e155713@ie.u-ryukyu.ac.jp>
2017-02-21 10:00:57 +0900

    add MM standerd

---
+++ Modeling/Modeling/MoneyManage.swift
@@ -0,0 +1,35 @@
+//
+//  MoneyManage.swift
+//  Modeling
+//
+//  Created by MameNitro on 2017/02/21.
+//  Copyright © 2017年 RyukyuUniversity. All rights reserved.
+//
+
+import UIKit
+
+class MoneyManage: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
